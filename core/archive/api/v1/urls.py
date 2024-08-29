694e29d08617ac556f9d2b4ce4e43617818d6142
hosseinpk <hosseinpakaein@gmail.com>
2024-08-29 13:54:51 +0330

    Revert "set media static"
    This reverts commit bdfd0a717cadb08982c496b17508fc11d497b6b6.

--- core/archive/api/v1/urls.py
+++ core/archive/api/v1/urls.py
@@ -9,7 +9,7 @@ router.register('archive',views.ArchiveView,basename='view')
 #router.register('archive-detail',views.ArchiveDetailView,basename='detail-view')
 router.register('category',views.CategoryApiView,basename='category')
 router.register('asset',views.AssetTypeApiView,basename='asset')
-router.register('project',views.ProjectApiView,basename='project')
+router.register('proejct',views.ProjectApiView,basename='project')
 
 
 urlpatterns = []
--- core/core/settings.py
+++ core/core/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = "core.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / 'templates'],
+        "DIRS": [],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -120,15 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = BASE_DIR / 'static'
-
-MEDIA_URL = 'media/'
-MEDIA_ROOT = BASE_DIR / 'media'
-
-STATICFILES_DIRS = [
-    BASE_DIR / 'statics'
-]
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
