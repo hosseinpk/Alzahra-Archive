68b2bfaa2df5139229e9612e3e2e15a7ce51f7cc
hosseinpk <hosseinpakaein@gmail.com>
2024-10-31 01:02:42 +0330

    Update settings.py

--- core/core/settings.py
+++ core/core/settings.py
@@ -190,7 +190,7 @@ from datetime import timedelta
 
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(days=7),
+    "ACCESS_TOKEN_LIFETIME": timedelta(hours=1),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
     "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": False,
@@ -224,7 +224,7 @@ SIMPLE_JWT = {
 
 
 # Celery Configuration Options
-CELERY_BROKER_URL = 'redis://localhost:6379/0'  # Use Redis as the message broker
+CELERY_BROKER_URL = 'redis://localhost:6379/0'  
 CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
