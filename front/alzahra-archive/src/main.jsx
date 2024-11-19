2cfd4bed1dd3c498c58c61a2f356ecff47e0395a
hosseinpk <hosseinpakaein@gmail.com>
2024-11-19 13:37:57 +0330

    finalize

--- front/alzahra-archive/src/main.jsx
+++ front/alzahra-archive/src/main.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
@@ -8,14 +7,3 @@ createRoot(document.getElementById("root")).render(
     <App />
   </Layout>
 );
-=======
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import { Layout } from './components/layout/layout';
-createRoot(document.getElementById('root')).render(
-       <Layout>
-    <App />
-    </Layout>
-)
->>>>>>> 5dccb62668fbd543209d7ba5954753387323079d
